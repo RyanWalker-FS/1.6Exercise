0f08e59ec64c135dfc69f1737372642c24d1d02a
RyanWalker-FS <ryanwalker490@gmail.com>
2025-02-06 19:11:06 -0500

    update

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1>Exercise 1.6</h1>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
