820c1fc8a1d65957f405e70655b835efd0a64662
RyanWalker-FS <ryanwalker490@gmail.com>
2025-02-06 21:45:17 -0500

    update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   output: "export",
   reactStrictMode: true,
   basePath: "/1.6Exercise",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/1.6Exercise/" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/1.6Exercise" : "",
 };
 
 module.exports = nextConfig;
