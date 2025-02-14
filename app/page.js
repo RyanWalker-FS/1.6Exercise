1a82facca6eefa269c7e8382ef22387df164badf
RyanWalker-FS <ryanwalker490@gmail.com>
2025-02-13 21:02:40 -0500

    update

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import DeployApi from "./deployapi";
 export default function Home() {
   return (
     <div>
       <h1>Exercise 1.6</h1>
       <button>
-        <DeployApi />
+        <Link to="/deployapi">Deploy API</Link>
       </button>
     </div>
   );
